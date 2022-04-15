5f851ddaf053d1b8f6df24d19e1ebde25e58aa4a
adevlinb <devlinbgenassembly@gmail.com>
2022-04-15 14:46:46 -0700

    one hour into tutorial

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
-	"example/user/hello/mascot"
 	"fmt"
 )
 
 func main() {
-	fmt.Println(mascot.BestMascot())
-	fmt.Println(quote.Go())
+	name := "Go Conference"
+	const conferenceTickets = -50
+	var remainingTickets uint = 47
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, name is %T \n", conferenceTickets, remainingTickets, name)
+
+	fmt.Printf("Welcome to our %v booking application\n", name)
+	fmt.Printf("We have a total of %v tickets and only %v left!\n", conferenceTickets, remainingTickets)
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	fmt.Print("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name:")
+	fmt.Scan(&lastName)
+	
+	fmt.Print("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("User %v %v booked %v tickets. You will receive a confirmation at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, name)
+
 }
 
-func TestMascot(t *testing.T) {
-	if mascot.BestMascot() != "Best" {
-		t.Fatal("wrong mascot")
-	}
-}
+
 
